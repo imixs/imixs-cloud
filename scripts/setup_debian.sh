5162056c9042bf6772ae4a96536308056c13017e
Kevin <kevin.yu@dreamtea.ca>
2023-08-19 10:32:39 -0600

    fix google cloud repo public key issue

--- scripts/setup_debian.sh
+++ scripts/setup_debian.sh
@@ -26,10 +26,9 @@ echo " adding k8s repositories..."
 echo "#############################################"
 apt-get update
 apt-get install -y gnupg curl
-# Add kubernetes repository  
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-
+# Add kubernetes repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.25/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.25/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 echo "#############################################"
 echo " configure prerequisites for container runtime"
